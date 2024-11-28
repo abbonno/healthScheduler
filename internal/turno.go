59e19ecdcceb0e69b940909a17136e9069ff97d4
mmnuria <mmnuria@correo.ugr.es>
2024-11-28 12:08:02 +0100

    Soluciona #8 añado const para almacenar los valores que se repiten y creo los turnos para cada tipo especifico.
    He reducido los archivos, ya que considero que eran innecesarios, además si el turno es nocturno tiene en cuenta que la hora de fin es al dia siguiente.

--- internal/turno.go
+++ internal/turno.go
@@ -1,31 +1,84 @@
 package health_scheduler
 
+import (
+	"fmt"
+	"time"
+)
+
+type TipoTurno string
+
+const (
+	Mañana TipoTurno = "Mañana"
+	Tarde  TipoTurno = "Tarde"
+	Noche  TipoTurno = "Noche"
+)
+
+type HorarioMañana string
+
+const (
+	InicioMañana HorarioMañana = "08:00"
+	FinMañana    HorarioMañana = "15:00"
+)
+
+type HorarioTarde string
+
+const (
+	InicioTarde HorarioTarde = "15:00"
+	FinTarde    HorarioTarde = "22:00"
+)
+
+type HorarioNoche string
+
+const (
+	InicioNoche HorarioNoche = "22:00"
+	FinNoche    HorarioNoche = "08:00"
+)
+
 type Turno struct {
-	Nombre TipoTurno
-	Inicio string
-	Fin    string
+	Empleado Empleado
+	Nombre   TipoTurno
+	Area     string
+	Fecha    string
+	Inicio   string
+	Fin      string
 }
 
-func NuevoTurno(nombre TipoTurno) *Turno {
-
-	var inicio, fin string
+func NuevoTurno(empleado Empleado, nombre TipoTurno, area, fecha string) *Turno {
+	var inicio, fin, fechaFin string
 
 	switch nombre {
 	case Mañana:
-		inicio = "08:00"
-		fin = "15:00"
+		inicio = string(InicioMañana)
+		fin = string(FinMañana)
+		fechaFin = fecha // Mismo día
 	case Tarde:
-		inicio = "15:00"
-		fin = "22:00"
+		inicio = string(InicioTarde)
+		fin = string(FinTarde)
+		fechaFin = fecha // Mismo día
 	case Noche:
-		inicio = "22:00"
-		fin = "08:00"
+		inicio = string(InicioNoche)
+		fin = string(FinNoche)
+		fechaFin = incrementarDia(fecha) // Día siguiente
+	default:
+		inicio, fin, fechaFin = "", "", fecha
 	}
 
 	return &Turno{
-		Nombre: nombre,
-		Inicio: inicio,
-		Fin:    fin,
+		Empleado: empleado,
+		Nombre:   nombre,
+		Area:     area,
+		Fecha:    fecha,
+		Inicio:   fmt.Sprintf("%s %s", fecha, inicio),
+		Fin:      fmt.Sprintf("%s %s", fechaFin, fin),
 	}
+}
 
+func incrementarDia(fecha string) string {
+
+	const layout = "2006-01-02"
+	parsedFecha, err := time.Parse(layout, fecha)
+	if err != nil {
+		return fecha
+	}
+	return parsedFecha.AddDate(0, 0, 1).Format(layout)
 }
