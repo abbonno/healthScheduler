75d9cc474bc66b1df9962fb95f56500500fcd316
mmnuria <mmnuria@correo.ugr.es>
2024-12-04 21:50:35 +0100

    Soluciona #8 reestructuración del turno
    Se ha simplificado la entidad Turno para que contenga el mínimo de información necesaria. Además se ha usado un tipo más apropiado para el campo Fecha y se ha eliminado el campo Empleado de la entidad.

--- internal/turno.go
+++ internal/turno.go
@@ -1,7 +1,6 @@
 package health_scheduler
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -13,48 +12,16 @@ const (
 	Noche  TipoTurno = "Noche"
 )
 
-var horarios = map[TipoTurno][2]string{
-	Mañana: {"08:00", "15:00"},
-	Tarde:  {"15:00", "22:00"},
-	Noche:  {"22:00", "08:00"},
-}
-
 type Turno struct {
-	Empleado    string
-	NombreTurno TipoTurno
-	Area        string
-	Fecha       string
-	Inicio      string
-	Fin         string
+	Nombre TipoTurno
+	Area   string
+	Fecha  time.Weekday
 }
 
-var RegistroTurnos []Turno // Lista de turnos asignados
-
-func NuevoTurno(empleado string, nombre TipoTurno, area, fecha string) *Turno {
-
-	inicio, fin := horarios[nombre][0], horarios[nombre][1]
-	fechaFin := fecha
-
-	if nombre == Noche {
-		fechaFin = incrementarDia(fecha) // Día siguiente para turno nocturno
-	}
-
+func NuevoTurno(nombre TipoTurno, area string, fecha time.Weekday) *Turno {
 	return &Turno{
-		Empleado:    empleado,
-		NombreTurno: nombre,
-		Area:        area,
-		Fecha:       fecha,
-		Inicio:      fmt.Sprintf("%s %s", fecha, inicio),
-		Fin:         fmt.Sprintf("%s %s", fechaFin, fin),
+		Nombre: nombre,
+		Area:   area,
+		Fecha:  fecha,
 	}
-}
-
-func incrementarDia(fecha string) string {
-
-	const layout = "2006-01-02"
-	parsedFecha, err := time.Parse(layout, fecha)
-	if err != nil {
-		return fecha
-	}
-	return parsedFecha.AddDate(0, 0, 1).Format(layout)
 }
