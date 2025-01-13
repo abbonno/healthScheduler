cc506d664eaf6628d7bafeb1976bf9133f97e45b
abbonno <abbogonluq@gmail.com>
2025-01-13 20:36:49 +0100

    Recupera estado anterior código #22
    Deshacemos cambios en el código para reestructurarlos y que se muestren adecuadamente en los issues

--- internal/turnos-asignados.go
+++ internal/turnos-asignados.go
@@ -1,8 +1,6 @@
 package health_scheduler
 
 import (
-	"errors"
-	"fmt"
 	"time"
 )
 
@@ -23,56 +21,3 @@ type Turno struct {
 type Empleado string
 
 type TurnosAsignados map[*Empleado][]Turno
-
-const totalAreas = 10
-const diasAnio = 365
-
-func GenerarPlanSemanal(enfermeros []Empleado) (TurnosAsignados, error) {
-	totalEmpleados := len(enfermeros)
-	if totalEmpleados < totalAreas {
-		return nil, errors.New("Hacen falta, al menos, 10 enfermeros, introducidos: " + fmt.Sprint(totalEmpleados))
-	}
-
-	turnosAsignados := make(map[*Empleado][]Turno)
-	areas := []string{"Área1", "Área2", "Área3", "Área4", "Área5", "Área6", "Área7", "Área8", "Área9", "Área10"}
-	tiposTurno := []TipoTurno{Mañana, Tarde, Noche}
-	indiceEnfermero := 0
-	fechaActual := time.Now()
-
-	for i := 0; i < diasAnio; i++ {
-		fecha := fechaActual.AddDate(0, 0, i)
-
-		for _, tipoTurno := range tiposTurno {
-			for _, area := range areas {
-				empleado := &enfermeros[indiceEnfermero]
-
-				turno := Turno{
-					Fecha:  fecha,
-					Area:   area,
-					Nombre: tipoTurno,
-				}
-
-				if _, ok := turnosAsignados[empleado]; !ok {
-					turnosAsignados[empleado] = []Turno{}
-				}
-
-				turnosAsignados[empleado] = append(turnosAsignados[empleado], turno)
-				indiceEnfermero = (indiceEnfermero + 1) % totalEmpleados
-			}
-		}
-	}
-
-	return turnosAsignados, nil
-}
-
-func GetTurnosEnfermero(enfermero *Empleado, turnosAsignados TurnosAsignados) []Turno {
-	return turnosAsignados[enfermero]
-}
-
-func TurnosToString(turnos []Turno) string {
-	var result string
-	for _, turno := range turnos {
-		result += "Fecha: " + turno.Fecha.Format("02-01-2006") + ", Turno: " + string(turno.Nombre) + ", Área: " + turno.Area + "\n"
-	}
-	return result
-}
