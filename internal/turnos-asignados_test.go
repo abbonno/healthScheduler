0e3a98d7e45f44c7d58090d98223bd5079662d17
abbonno <abbogonluq@gmail.com>
2025-01-27 12:54:26 +0100

    Modulariza comportamiento de los tests #31 #32
    Aplicamos el principio DRY para evitar repetición de código creando el dato Ocupación que guarda el número de enfermeros en cada área en cada momento

--- internal/turnos-asignados_test.go
+++ internal/turnos-asignados_test.go
@@ -6,6 +6,44 @@ import (
 	"time"
 )
 
+type Ocupacion map[time.Time]map[TipoTurno]map[string]int
+
+func NewOcupacion(plan TurnosAsignados) Ocupacion {
+	ocupacion := make(map[time.Time]map[TipoTurno]map[string]int)
+	for _, turnos := range plan {
+		for _, turno := range turnos {
+			fechaNormalizada := turno.Fecha
+			if ocupacion[fechaNormalizada] == nil {
+				ocupacion[fechaNormalizada] = make(map[TipoTurno]map[string]int)
+			}
+			if ocupacion[fechaNormalizada][turno.Nombre] == nil {
+				ocupacion[fechaNormalizada][turno.Nombre] = make(map[string]int)
+			}
+			ocupacion[fechaNormalizada][turno.Nombre][turno.Area]++
+		}
+	}
+	return ocupacion
+}
+
+func TestAreaOcupadaDoblemente(t *testing.T) {
+	totalEnfermeros := 15
+	enfermeros := make([]Empleado, totalEnfermeros)
+	for i := 1; i <= totalEnfermeros; i++ {
+		e := Empleado("Enfermero" + strconv.Itoa(i))
+		enfermeros[i-1] = e
+	}
+	plan := GenerarPlanAnual(enfermeros)
+
+	listaOcupacion := NewOcupacion(plan)
+	for _, turnos := range plan {
+		for _, turno := range turnos {
+			if listaOcupacion[turno.Fecha][turno.Nombre][turno.Area] > 1 {
+				t.Fatalf("Área duplicada: el área %s ya está ocupada el día %v en el turno %v", turno.Area, turno.Fecha, turno.Nombre)
+			}
+		}
+	}
+}
+
 func TestAreaSinOcupar(t *testing.T) {
 	totalEnfermeros := 15
 	enfermeros := make([]Empleado, totalEnfermeros)
@@ -19,56 +57,17 @@ func TestAreaSinOcupar(t *testing.T) {
 		areas[i-1] = "Area" + strconv.Itoa(i)
 	}
 	turnosAsignados := GenerarPlanAnual(enfermeros)
-	ocupacion := make(map[time.Time]map[TipoTurno]map[string]bool)
-	for _, turnos := range turnosAsignados {
-		for _, turno := range turnos {
-			fechaNormalizada := turno.Fecha
-			if ocupacion[fechaNormalizada] == nil {
-				ocupacion[fechaNormalizada] = make(map[TipoTurno]map[string]bool)
-			}
-			if ocupacion[fechaNormalizada][turno.Nombre] == nil {
-				ocupacion[fechaNormalizada][turno.Nombre] = make(map[string]bool)
-			}
-			ocupacion[fechaNormalizada][turno.Nombre][turno.Area] = true
-		}
-	}
 
+	listaOcupacion := NewOcupacion(turnosAsignados)
 	fechaInicio := time.Now()
 	for i := 0; i < 365; i++ {
 		fecha := fechaInicio.AddDate(0, 0, i)
 		for _, tipoTurno := range tiposTurno {
 			for _, area := range areas {
-				if ocupacion[fecha][tipoTurno][area] == false {
+				if listaOcupacion[fecha][tipoTurno][area] == 0 {
 					t.Fatalf("Área %s no ocupada el %s en el turno %s", area, fecha.Format("02-01-2006"), tipoTurno)
 				}
 			}
-		}
-	}
-
-}
-
-func TestAreaOcupadaDoblemente(t *testing.T) {
-	totalEnfermeros := 15
-	enfermeros := make([]Empleado, totalEnfermeros)
-	for i := 1; i <= totalEnfermeros; i++ {
-		e := Empleado("Enfermero" + strconv.Itoa(i))
-		enfermeros[i-1] = e
-	}
-	plan := GenerarPlanAnual(enfermeros)
-	ocupacion := make(map[time.Time]map[TipoTurno]map[string]bool)
-	for _, turnos := range plan {
-		for _, turno := range turnos {
-			fechaNormalizada := turno.Fecha
-			if ocupacion[fechaNormalizada] == nil {
-				ocupacion[fechaNormalizada] = make(map[TipoTurno]map[string]bool)
-			}
-			if ocupacion[fechaNormalizada][turno.Nombre] == nil {
-				ocupacion[fechaNormalizada][turno.Nombre] = make(map[string]bool)
-			}
-			if ocupacion[fechaNormalizada][turno.Nombre][turno.Area] == true {
-				t.Fatalf("Área duplicada: el área %s ya está ocupada el día %v en el turno %v", turno.Area, turno.Fecha, turno.Nombre)
-			}
-			ocupacion[fechaNormalizada][turno.Nombre][turno.Area] = true
 		}
 	}
 }
