4728a05d40a90199fff4952db1758ab47021a449
abbonno <abbogonluq@gmail.com>
2025-01-27 12:57:13 +0100

    Completa función GenerarPlanAnual #30
    Tras completar los tests a tener en cuenta, completamos la función de creación del plan siguiendo el TDD

--- internal/turnos-asignados.go
+++ internal/turnos-asignados.go
@@ -1,6 +1,7 @@
 package health_scheduler
 
 import (
+	"strconv"
 	"time"
 )
 
@@ -25,5 +26,25 @@ type TurnosAsignados map[*Empleado][]Turno
 const totalAreas = 10
 const diasPlan = 365
 
-func GenerarPlanAnual() TurnosAsignados {
+func GenerarPlanAnual(enfermeros []Empleado) TurnosAsignados {
+	turnosAsignados := make(map[*Empleado][]Turno)
+	fechaActual := time.Now()
+	tiposTurno := []TipoTurno{Mañana, Tarde, Noche}
+	areas := make([]string, totalAreas)
+	for i := 1; i <= totalAreas; i++ {
+		areas[i-1] = "Area" + strconv.Itoa(i)
+	}
+	indiceEnfermero := 0
+
+	for i := 0; i < diasPlan; i++ {
+		fecha := fechaActual.AddDate(0, 0, i)
+		for _, tipoTurno := range tiposTurno {
+			for _, area := range areas {
+				turno := Turno{Fecha: fecha, Area: area, Nombre: tipoTurno}
+				turnosAsignados[&enfermeros[indiceEnfermero]] = append(turnosAsignados[&enfermeros[indiceEnfermero]], turno)
+				indiceEnfermero = (indiceEnfermero + 1) % len(enfermeros)
+			}
+		}
+	}
+	return turnosAsignados
 }
