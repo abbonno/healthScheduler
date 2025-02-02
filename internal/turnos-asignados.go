0642852b4e26c7a7483cfb02aee7f742efd4c10c
abbonno <abbogonluq@gmail.com>
2025-02-02 20:28:26 +0100

    Closes #30; closes #31; closes #32; closes #33; closes #34; closes #35 rehacer issues

--- internal/turnos-asignados.go
+++ internal/turnos-asignados.go
@@ -1,7 +1,6 @@
 package health_scheduler
 
 import (
-	"strconv"
 	"time"
 )
 
@@ -16,48 +15,9 @@ const (
 type Turno struct {
 	Nombre TipoTurno
 	Area   string
-	Fecha  time.Time
+	Fecha  time.Weekday
 }
 
 type Empleado string
 
 type TurnosAsignados map[*Empleado][]Turno
-
-const totalAreas = 10
-const diasPlan = 365
-
-func NormalizarFecha(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
-}
-
-func GenerarPlanAnual(enfermeros []Empleado) TurnosAsignados {
-	turnosAsignados := make(map[*Empleado][]Turno)
-	fechaActual := NormalizarFecha(time.Now())
-	tiposTurno := []TipoTurno{Mañana, Tarde, Noche}
-	areas := make([]string, totalAreas)
-	for i := 1; i <= totalAreas; i++ {
-		areas[i-1] = "Area" + strconv.Itoa(i)
-	}
-	indiceEnfermero := 0
-
-	for i := 0; i < diasPlan; i++ {
-		fecha := fechaActual.AddDate(0, 0, i)
-		for _, tipoTurno := range tiposTurno {
-			for _, area := range areas {
-				turno := Turno{Fecha: fecha, Area: area, Nombre: tipoTurno}
-				turnosAsignados[&enfermeros[indiceEnfermero]] = append(turnosAsignados[&enfermeros[indiceEnfermero]], turno)
-				indiceEnfermero = (indiceEnfermero + 1) % len(enfermeros)
-			}
-		}
-	}
-	return turnosAsignados
-}
-
-func MostrarTurnosEnfermero(enfermero *Empleado, plan TurnosAsignados) string {
-	turnos := plan[enfermero]
-	var resultado string
-	for _, turno := range turnos {
-		resultado += "Fecha: " + turno.Fecha.Format("02-01-2006") + ", Turno: " + string(turno.Nombre) + ", Área: " + turno.Area + "\n"
-	}
-	return resultado
-}
